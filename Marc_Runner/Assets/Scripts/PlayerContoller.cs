acede518c72628eba68c024c503f8cac681b4fdb
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-15 19:47:29 +0200

    Started Project

---
+++ Marc_Runner/Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerRB.AddForce(Vector3.up * 1000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
