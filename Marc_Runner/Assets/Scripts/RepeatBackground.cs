79f879a6e2359b9c25b825b3fa5f1719d2043592
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-29 19:48:13 +0100

    Obstacles & Background

---
+++ Marc_Runner/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos; // position at the 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position; // put start position as it's current one (same position to avoid hardcoding)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < startPos.x -50) // if it's too much to the left
+        {
+            transform.position = startPos; // put the current position to the start one
+        }
+    }
+}
---
+++ Marc_Runner/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefab; // the obstacle prefab that spawns
+    public int obstacleIndex; // the index of the GameObject in the Array
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0); // the position it will spawn
+
+    private float startDelay = 2; // the seconds of the delay of the spawn after the start function
+    private float repeatRate = 2; // the interval it will be called
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate); // call the function constantly
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnObstacle ()
+    {
+        int obstacleIndex = Random.Range(0, obstaclePrefab.Length);
+        Instantiate(obstaclePrefab[obstacleIndex], spawnPos, obstaclePrefab[obstacleIndex].transform.rotation); //spawn obstacle
+    }
+}
