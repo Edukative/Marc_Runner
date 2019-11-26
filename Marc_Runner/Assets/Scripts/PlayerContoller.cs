2c52da6643ddfa643f820abadcc7c831d5a43aa3
Edukative <56083723+Edukative@users.noreply.github.com>
2019-11-26 19:51:11 +0100

    Revert "Revert "Minor change""
    This reverts commit 60559e148236c5ab4062a3b1bf4927da1131683c.

--- Marc_Runner/Assets/Scripts/PlayerContoller.cs
+++ Marc_Runner/Assets/Scripts/PlayerContoller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerContoller : MonoBehaviour
 {
     private Rigidbody playerRB;
+    private Animator playerAnim;
 
     public float jumpForce; // the force the player jumps
     public float gravityModifier; // to modify the gravity, to earth one to a lunar one!
@@ -17,6 +18,11 @@ public class PlayerContoller : MonoBehaviour
     private SpriteRenderer hp1;
     private SpriteRenderer hp2;
     private SpriteRenderer hp3;
+
+    // Particles
+
+    public ParticleSystem explosion; // the explosion Particle
+    public ParticleSystem dirt; // the dirt particle when the player runs
     // Start is called before the first frame update
     void Start()
     {
@@ -26,9 +32,9 @@ public class PlayerContoller : MonoBehaviour
         Physics.gravity *= gravityModifier; //Modify the default Unity gravity to your gravity!
 
         GameObject canvas = GameObject.Find("Canvas");
-        hp1 = canvas.transform.GetChild(0).GetComponent<SpriteRenderer>();
-        hp2 = canvas.transform.GetChild(1).GetComponent<SpriteRenderer>();
-        hp3 = canvas.transform.GetChild(2).GetComponent<SpriteRenderer>();
+        hp1 = GameObject.Find("HP1").GetComponent<SpriteRenderer>();
+        hp2 = GameObject.Find("HP2").GetComponent<SpriteRenderer>();
+        hp3 = GameObject.Find("HP3").GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -38,6 +44,12 @@ public class PlayerContoller : MonoBehaviour
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); //Apply a impulse force up, to make it jump
             isOnGround = false; // no longer touches de ground
+
+            // Animations
+            playerAnim.SetTrigger("Jump_trig");
+
+            // Particles
+            dirt.Stop(); // stop playing the particle
         }
     }
 
@@ -47,16 +59,25 @@ public class PlayerContoller : MonoBehaviour
         Debug.Log(hp);
         switch (hp) //hp cases
         {
-            case 2: hp3.SetActive(false); // if hp is 2
+            case 2: hp3.gameObject.SetActive(false); // if hp is 2
                 break;
-            case 1: hp2.SetActive(false); // if hp is 1
+            case 1: hp2.gameObject.SetActive(false); // if hp is 1
                 break;
-            case 0: hp1.SetActive(false); // if hp is 0
+            case 0: hp1.gameObject.SetActive(false); // if hp is 0
                 isGameOver = true;
+               
+                // Animations
+                playerAnim.SetBool("Death_b", true);
+                playerAnim.SetInteger("DeathType_int", 1);
+               
+                // Particles
+                explosion.Play(); // Play the explosion particle
+                dirt.Stop(); // stop playing the particle
+
                 break;
-            default: hp3.SetActive(true); // default case that is necessary
-                     hp2.SetActive(true);
-                     hp1.SetActive(true);
+            default: hp3.gameObject.SetActive(true); // default case that is necessary
+                     hp2.gameObject.SetActive(true);
+                     hp1.gameObject.SetActive(true);
                 break;
         }
     }
@@ -66,6 +87,7 @@ public class PlayerContoller : MonoBehaviour
         if(collision.gameObject.CompareTag("Ground")) // or the player collides with the ground
         {
             isOnGround = true;
+            dirt.Play(); // play again the particle
         }
         else if (collision.gameObject.CompareTag("Obstacle")) // or the player collides with an obstacle
         {
